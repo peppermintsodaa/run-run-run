962315c1a5da162cd16ddf3875e7189138872d65
peppermintsodaa <70208003+peppermintsodaa@users.noreply.github.com>
2021-10-01 22:44:11 -0400

    moved files to main directory

---
+++ desktop/src/com/cheese/desktop/DesktopLauncher.java
@@ -0,0 +1,16 @@
+package com.cheese.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl.LwjglCanvas;
+import com.cheese.Project2;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1280;
+		config.height = 720;
+		config.title = "simply running away from my responsibilities";
+		new LwjglApplication(new Project2(), config);
+	}
+}
